54efafc532f02ff49fe3bdc2be3ffda3fb69b065
robinzhx <zhx068@ucsd.edu>
2019-11-10 21:25:10 -0800

    simple shaking test & upload the pre-scan meshes

---
+++ shakAR/Assets/ObjectShaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectShaker : MonoBehaviour
+{
+    public float speed = 1.0f; //how fast it shakes
+    public float amount = 1.0f; //how much it shakes
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3 (Mathf.Sin(Time.time * speed) * amount, 0, 0);
+    }
+}
