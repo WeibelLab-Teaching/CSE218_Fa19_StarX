9e9d91c8ca1a97275625b3d518f6ba557dd67314
Anusha Kopparam <akoppara@UCSD.EDU>
2019-11-15 18:27:34 -0800

    adding timer, enable pause/continue, wip mrtk

---
+++ Assets/Scripts/summaryText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class summaryText : MonoBehaviour
+{
+    public Text timeTaken;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeTaken.text = "Time taken: " + Timer.textClock.text;
+    }
+}
