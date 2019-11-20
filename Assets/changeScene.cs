56b49a6f470a2aba66dd1cfa124a284b8819f6f3
Anusha Kopparam <una.020@gmail.com>
2019-11-19 19:56:12 -0800

    added mrtk support, onclick scene transition

---
+++ Assets/changeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void changeMenuScene(string sceneName) {
+        // Debug.Log("Clicked");
+        // Debug.Log(sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+}
