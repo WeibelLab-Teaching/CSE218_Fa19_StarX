83a6ace5c952f14316000f7dbe946b9631cc4b95
Weiwei Zhou <w1zhou@UCSD.EDU>
2019-11-23 22:14:01 -0800

    add a new scene to integrate multiple scenes

---
+++ Assets/Scripts/rotater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotater : MonoBehaviour
+{
+    public Vector3 rotateAnglePerFrame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 oldAngles = this.gameObject.transform.eulerAngles;
+        this.gameObject.transform.eulerAngles = new Vector3(oldAngles.x + rotateAnglePerFrame.x, oldAngles.y + rotateAnglePerFrame.y, oldAngles.z + rotateAnglePerFrame.z);
+    }
+}
