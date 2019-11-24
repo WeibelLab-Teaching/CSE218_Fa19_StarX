4bfbfe3c1b0661efd1ff45fd73ea09b7819e5cc0
Weiwei Zhou <w1zhou@UCSD.EDU>
2019-11-23 22:14:01 -0800

    add a new scene to integrate multiple scenes

---
+++ Assets/Scripts/objectManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectManager : MonoBehaviour
+{
+    // Update is called once per frame
+    public void scanToLevel(GameObject activate)
+    {
+        activate.SetActive(true);
+        this.transform.parent.parent.gameObject.SetActive(false);
+    }
+}
