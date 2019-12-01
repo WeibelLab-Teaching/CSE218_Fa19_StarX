e1120333d23ae13f37d8a726e7a8090e9a7d43b5
Anusha Kopparam <akoppara@UCSD.EDU>
2019-11-30 16:51:21 -0800

    adding interactable safe place and danger place icon (work in progress)

---
+++ Assets/Scripts/correctCounter.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class summaryText : MonoBehaviour
+{
+    public TextMeshPro timeTaken;
+    // Start is called before the first frame update
+    public static bool firstIntoSummary = true;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstIntoSummary)
+        {
+            showlast();
+            firstIntoSummary = false;
+        }
+       
+    }
+
+    public void setFlag()
+    {
+        firstIntoSummary = true;
+    }
+    public void showlast()
+    {
+        float overallTime = timerController.idealSecond + timerController.idealMinute * 60;
+        float timeleft = timerController.second + timerController.minute * 60;
+        float TimeUsed = overallTime - timeleft;
+        Debug.Log(overallTime);
+        Debug.Log(timeleft);
+        Debug.Log(TimeUsed);
+        float minuteUsed = TimeUsed / 60;
+        float secondUsed = TimeUsed % 60;
+        Debug.Log(LeadingZero(minuteUsed));
+        Debug.Log(LeadingZero(secondUsed));
+        string text = LeadingZero(minuteUsed) + ':' + LeadingZero(secondUsed);
+        timeTaken.text = "CORRECT: " + scoreKeeper.correctCount.ToSTring();
+    }
+
+    public string LeadingZero(float n)
+    {
+        return ((int)n).ToString().PadLeft(2, '0');
+    }
+}
---
+++ Assets/Scripts/scoreKeeper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scoreKeeper : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public static int mistakeCount;
+	public static int correctCount;
+    void Start()
+    {
+        mistakeCount = 0;
+		correctCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void incMistake(){
+		mistakeCount += 1;
+	}
+	
+	public void incCorrect(){
+		correctCount += 1;
+	}
+}
+
