fb2cc0c5fe5046d70c7ac1c387e1840d2e4f08ef
davidzhangxm <xiz015@ucsd.edu>
2019-12-02 19:11:27 -0800

    physics object added

---
+++ Assets/ColliderController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EZCameraShake;
+
+public class ColliderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public shaketest cameraShake;
+    bool detect;
+    public CameraShaker shaker;
+    public float magniture;
+    public List<Collider> colliderToDetect;
+    bool hasNoNullObject = true;
+
+    public AudioClip warningClip;
+    AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    private void Start()
+    {
+        detect = false;
+        foreach (Collider c in this.GetComponentsInChildren<Collider>())
+        {
+            colliderToDetect.Add(c);
+        }
+        for (int i = 0; i < colliderToDetect.Count; i++)
+        {
+            Collider c = colliderToDetect[i];
+            if (!c)
+            {
+                Debug.LogError("Collider in index " + i + " is null");
+                hasNoNullObject = false;
+            } else
+            {
+                c.gameObject.AddComponent<CollideTrigger>().setController(this);
+    
+            }
+        }
+
+
+        if (!shaker)
+        {
+            shaker = FindObjectOfType<CameraShaker>();
+            Debug.LogWarning("Did not assigned CameraShaker, find one " + shaker.name);
+            if (!shaker)
+            {
+                Debug.LogError("Can not find a CameraShaker!");
+                hasNoNullObject = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+
+        if (detect)
+        {
+            //cameraShake.Shake(1f, .4f);
+            //shaker.ShakeOnce(1.0f, 1.0f, 1.0f, 1.0f);
+            detect = false;
+        }
+    }
+    public void trigerEvent()
+    {
+        detect = true;
+        playAudio();
+        shaker.ShakeOnce(magniture, 1.0f, 1.0f, 1.0f);
+        Debug.Log("Here");
+    }
+
+    public void playAudio()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = warningClip;
+        source.playOnAwake = false;
+        PlaySound();
+    }
+
+    void PlaySound()
+    {
+        source.PlayOneShot(warningClip);
+    }
+
+}
---
+++ Assets/Scripts/CollideTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using EZCameraShake;
+public class CollideTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public shaketest cameraShake;
+    ColliderController controller;
+
+    private void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    public void setController(ColliderController c)
+    {
+        this.controller = c;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Ground"))
+            controller.trigerEvent();
+    }
+}
